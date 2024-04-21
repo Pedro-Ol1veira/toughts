a9866bec3ea2d4a9224ee4a5b33b6db4e6b878e3
Pedro-Ol1veira <113263052+Pedro-Ol1veira@users.noreply.github.com>
2024-04-20 22:45:19 -0300

    middleweare de autenticacao

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -4,5 +4,13 @@ const User = require('../models/User');
 module.exports = class ToughtsController {
     static async showToughts(req, res) {
         res.render('tought/home');
-    }
+    };
+
+    static async dashboard(req, res) {
+        res.render('tought/dashboard');
+    };
+
+    static createTought(req, res) {
+        res.render('tought/create');
+    };
 };

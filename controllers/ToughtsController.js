d1b51e83f211d1d5b00f0aee07247e6bc7fb827c
Pedro-Ol1veira <113263052+Pedro-Ol1veira@users.noreply.github.com>
2024-05-08 21:36:06 -0300

    edit route

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,9 +1,16 @@
+const { where } = require('sequelize');
 const Tought = require('../models/Toughts');
 const User = require('../models/User');
 
 module.exports = class ToughtsController {
     static async showToughts(req, res) {
-        res.render('tought/home');
+        const toughtsData = await Tought.findAll({
+            include:User,
+        });
+
+        const toughts = toughtsData.map((result) => result.get({plain: true}));
+
+        res.render('tought/home', {toughts});
     };
 
     static async dashboard(req, res) {
@@ -16,14 +23,20 @@ module.exports = class ToughtsController {
             include: Tought,
             plain: true,
         });
-
+        
         if(!user) {
             res.redirect('/login');
         }
 
         const toughts = user.Toughts.map((result) => result.dataValues);
 
-        res.render('tought/dashboard', { toughts });
+        let emptyToughts = false;
+
+        if (toughts.length === 0) {
+            emptyToughts = true;
+        }
+
+        res.render('tought/dashboard', { toughts, emptyToughts });
     };
 
     static createTought(req, res) {
@@ -47,5 +60,47 @@ module.exports = class ToughtsController {
         } catch (error) {
             console.log(error);
         }
+    };
+
+    static async removeTought(req, res) {
+        const id = req.body.id;
+        const userId = req.session.userid;
+
+        try {
+            await Tought.destroy({where: {id: id, UserId: userId}});
+
+            req.flash('message', 'Pensamento removido com sucesso!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
+    static async editTought(req, res) {
+        const id = req.params.id;
+
+        const tought = await Tought.findOne({where: {id: id}, raw:true});
+
+        res.render('tought/edit', {tought});
+    };
+
+    static async postEdit(req, res) {
+        const id = req.body.id;
+        const tought = {
+            title: req.body.title,
+        };
+
+        try {
+            await Tought.update(tought ,{where: {id: id}});
+            req.flash('message', 'Pensamento atualizado com sucesso!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
     };
 };
--- routes/toughtsRouter.js
+++ routes/toughtsRouter.js
@@ -8,7 +8,10 @@ const checkAuth = require('../helpers/auth').checkAuth;
 
 router.get('/add', checkAuth, ToughtsController.createTought);
 router.post('/add', checkAuth, ToughtsController.createToughtSave);
+router.get('/edit/:id', checkAuth, ToughtsController.editTought);
+router.post('/edit', checkAuth, ToughtsController.postEdit)
 router.get('/dashboard', checkAuth, ToughtsController.dashboard);
+router.post('/remove', checkAuth, ToughtsController.removeTought);
 router.get('/', ToughtsController.showToughts);
 
 
